eab65c4422c261a245d25beb938331f8f994e76c
ItaruYamaguchi <i-yamaguchi@crearaize.com>
2016-11-19 15:21:33 +0900

    yama

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var oka04: Int
 
+  var yama: String
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
